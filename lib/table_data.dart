1f5fa667fc7fe77d152c32a0724d9e9ca16542f2
KarolKmak <00grunt@gmail.com>
2024-09-21 15:44:52 +0200

    Init commit

---
+++ lib/table_data.dart
@@ -0,0 +1,13 @@
+class  UserStandings {
+  String name = "empty";
+  String championbet = "empty";
+  String topscorer = "empty";
+  int points = 0;
+
+  UserStandings({
+    required this.name,
+    required this.championbet,
+    required this.topscorer,
+    required this.points,
+});
+}
