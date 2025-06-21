9467463827ef537d3b4c2beabd6d183a47ea1267
KarolKmak <58179517+KarolKmak@users.noreply.github.com>
2025-06-21 21:57:49 +0200

    Update GetMainTable.js

--- functions/API/GetMainTable.js
+++ functions/API/GetMainTable.js
@@ -1,6 +1,6 @@
 export async function onRequestGet()
 {
-  const stmt = env.obstawiatorDB.prepare("SELECT UserScores.championBet, UserScores.topScorerBet, UserScores.points, Users.name FROM UserScores INNER JOIN Users ON UserScores.userID=Users.ID ORDER BY UserScores.points DESC"); 
+  const stmt = context.env.obstawiatorDB.prepare("SELECT UserScores.championBet, UserScores.topScorerBet, UserScores.points, Users.name FROM UserScores INNER JOIN Users ON UserScores.userID=Users.ID ORDER BY UserScores.points DESC"); 
   const returnValue = await stmt.run();
   return Response.json(returnValue.results)
 }
