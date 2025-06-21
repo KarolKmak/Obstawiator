1df581ce5ada26fc15d9860eb0e4e33da3c59fb9
KarolKmak <58179517+KarolKmak@users.noreply.github.com>
2025-06-21 21:48:24 +0200

    Delete functions/API.js

--- functions/API.js
+++
@@ -1,8 +0,0 @@
-export function onRequest(context) {
-   const url = new URL(context.request.url);
-   if (url.pathname === "/test")
-   {
-     return new Response("test");
-   }
-  return new Response("Hello, world!");
-}
