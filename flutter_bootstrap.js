238fc72d6c02c0905e70f3fa6d60763e59f15a42
KarolKmak <00grunt@gmail.com>
2025-07-09 21:05:37 +0200

    Allow updating initial bet with point deduction if not too late
    - If the current time is before a specific deadline (1752692400000) and the user exists:
        - Check if the user's initial bet has been changed before.
        - If not changed or `betChanged` is null, update the bet, deduct 5 points, and set `betChanged` to 1.
        - If already changed, update the bet without point deduction.
    - If the current time is past the initial deadline (1751472000000) but before the extended deadline, and the user doesn't meet the above conditions for updating, the existing "too late" error is returned.
    - If the current time is past the extended deadline, the "too late" error is returned.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3747078798"
+    serviceWorkerVersion: "2900835635"
   }
 });
--- functions/API/InitialBets.js
+++ functions/API/InitialBets.js
@@ -14,6 +14,21 @@ export async function onRequestPost(context)
   const checkIDResult = await checkID.run();
   if(Date.now() > 1751472000000)
   {
+    if(Date.now() < 1752692400000 && checkIDResult.results.length>0)
+    {
+        const checkIfChanged = context.env.obstawiatorDB.prepare("SELECT betChanged FROM UserScores WHERE ID = ?").bind(reqBody.ID);
+        const checkIfChangedResult = await checkIfChanged.run();
+        if(checkIfChangedResult.results[0].betChanged == 0 || checkIfChangedResult.results[0].betChanged == null)
+        {
+            const stmt = context.env.obstawiatorDB.prepare("UPDATE UserScores SET betChanged = 1, points = points - 5, championBet = ? WHERE ID = ?").bind(reqBody.championBet, reqBody.ID);
+            const result = await stmt.run();
+            return Response.json({message:"Pomyślnie aktualizowano zakład, odjęto 5pkt"}, {status: 201});
+        }
+        const stmt = context.env.obstawiatorDB.prepare("UPDATE UserScores SET championBet = ? WHERE ID = ?").bind(reqBody.championBet, reqBody.ID);
+        const result = await stmt.run();
+        return Response.json({message:"Pomyślnie aktualizowano zakład"}, {status: 201});
+    }
+
     return Response.json({message: "Nie udało się dodać zakładu, za późno"}, {status: 403});
   }
 
