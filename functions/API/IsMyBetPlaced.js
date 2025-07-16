9efa5a4bc5816d1eed12669811fe942dd31dcae7
KarolKmak <00grunt@gmail.com>
2025-07-16 18:20:56 +0200

    Check if user exists before checking for bet
    This commit modifies the `IsMyBetPlaced` API endpoint to first verify the existence of the user before checking if they have placed a bet on the specified match.
    
    If the user exists and has placed a bet, the API now returns the bet details including the `winner`.

--- functions/API/IsMyBetPlaced.js
+++ functions/API/IsMyBetPlaced.js
@@ -9,15 +9,20 @@ export async function onRequestPost(context)
   {
     return Response.redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ", 302);
   }
+  // Check if user exists
   const checkUserID = context.env.obstawiatorDB.prepare("SELECT ID FROM Users WHERE ID = ?").bind(reqBody.ID);
   const checkResult = await checkUserID.run();
   console.log(checkResult.results);
+  // If user exists
   if(checkResult.results.length>0)
   {
-    const getUserBet = context.env.obstawiatorDB.prepare("SELECT matchID FROM BetMatch WHERE userID = ? AND matchID = ?").bind(reqBody.ID, reqBody.matchID);
+    // Check if user has placed a bet for the given match
+    const getUserBet = context.env.obstawiatorDB.prepare("SELECT matchID, winner FROM BetMatch WHERE userID = ? AND matchID = ?").bind(reqBody.ID, reqBody.matchID);
     const getUserBetResult = await getUserBet.run();
+    // If user has placed a bet
     if(getUserBetResult.results.length>0)
     {
+        // Return the bet
         return Response.json({userBet: getUserBetResult.results[0]}, {status: 200});
     }
     else
