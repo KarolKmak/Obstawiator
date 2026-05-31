a6e18d6c9e8326662926ea587d07e41e7368f7be
KarolKmak <00grunt@gmail.com>
2026-05-31 19:34:23 +0200

    Refactor functions: add secure password hashing and validation

---
+++ android/app/src/main/kotlin/com/example/obstawiator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.obstawiator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/main_table/table_data.dart
@@ -0,0 +1,15 @@
+class  UserStandings {
+  String name = "empty";
+  String championbet = "empty";
+  String topscorer = "empty";
+  int points = 0;
+  int ID;
+
+  UserStandings({
+    required this.name,
+    required this.championbet,
+    required this.topscorer,
+    required this.points,
+    required this.ID,
+});
+}
